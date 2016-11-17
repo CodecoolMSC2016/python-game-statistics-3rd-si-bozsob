524a794988017ee0c62401dcd8b18bf0e32860c2
Trixi <bozso.beatrix@gmail.com>
2016-11-17 13:33:39 +0100

    trixi did

--- printing.py
+++ printing.py
@@ -10,7 +10,6 @@ print(reports.count_by_genre(file_name, "First-person shooter"))
 print(reports.get_line_number_by_title(file_name, "Counter-Strike"))
 print(reports.get_genres(file_name))
 print(reports.when_was_top_sold_fps(file_name))
-
 #print(reports.sort_abc(file_name)) 
 for i in reports.sort_abc(file_name):
     print(i)
